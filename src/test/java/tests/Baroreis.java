7fb07cebe402d28cca4435a277b5ba32679893b5
Baros2535 <brsaltn.25@gmail.com>
2021-06-30 00:01:41 +0300

    before ve after methodumu tamamladım

--- src/test/java/tests/Baroreis.java
+++ src/test/java/tests/Baroreis.java
@@ -1,16 +1,24 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
 
 public class Baroreis {
+    WebDriver driver;
     @BeforeMethod
     public void setUp(){
+   driver = WebDriverFactory.getDriver("chorome");
+   driver.manage().window().maximize();
+
 
     }
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
 
 
     }
