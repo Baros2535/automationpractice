245fbb4c4eb55e4d662cbd9a18ff4d554d1835a2
johnolliverjr <johnolliverjr@gmail.com>
2021-06-30 01:05:58 +0300

    after method

--- src/test/java/tests/Team1TestCase1_Erhan.java
+++ src/test/java/tests/Team1TestCase1_Erhan.java
@@ -1,7 +1,12 @@
 package tests;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
 public class Team1TestCase1_Erhan {
@@ -11,5 +16,10 @@ public class Team1TestCase1_Erhan {
     public void setUp(){
         driver = WebDriverFactory.getDriver("chrome");
     }
+    @AfterMethod
+    public void tearDown()  {
+        driver.quit();
+    }
+
 
 }
