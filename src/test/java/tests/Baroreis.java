91d9bff7e9fe9acbc5b0a0351c8ecaf0b757f609
Baros2535 <brsaltn.25@gmail.com>
2021-06-29 23:58:39 +0300

    branch ve sınıfımı oluşturdum

---
+++ src/test/java/tests/Baroreis.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Baroreis {
+    @BeforeMethod
+    public void setUp(){
+
+    }
+    @AfterMethod
+    public void tearDown(){
+
+
+    }
+    @Test
+    public void test1(){
+
+    }
+
+}
