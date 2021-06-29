e1ba14475ae9a84eacf87a9504ced1f28411694b
johnolliverjr <johnolliverjr@gmail.com>
2021-06-30 01:06:39 +0300

    test

--- src/test/java/tests/Team1TestCase1_Erhan.java
+++ src/test/java/tests/Team1TestCase1_Erhan.java
@@ -20,6 +20,22 @@ public class Team1TestCase1_Erhan {
     public void tearDown()  {
         driver.quit();
     }
+    @Test
+    public void openBrowser() throws InterruptedException {
+
+        driver.get("http://automationpractice.com/index.php");
+        driver.findElement(By.xpath("//ul[@id='homefeatured']/li[@class='ajax_block_product col-xs-12 col-sm-4 col-md-3 first-in-line first-item-of-tablet-line first-item-of-mobile-line']//span[.='Add to cart']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
+
+        String popupPrice = driver.findElement(By.xpath("(//*[text() = '$18.51'])[2]")).getText();
+
+        String checkoutPrice = driver.findElement(By.xpath("//*[@id=\"total_price\"]")).getText();
+
+        Assert.assertEquals(popupPrice,checkoutPrice,"verify text is same");
+
+        driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]/span")).click();
+    }
 
 
 }
