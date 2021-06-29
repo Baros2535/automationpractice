bcc93111c1051d523cafa6922cce3d3fe702a283
johnolliverjr <johnolliverjr@gmail.com>
2021-06-30 00:54:16 +0300

    before method

--- src/test/java/tests/Team1TestCase1_Erhan.java
+++ src/test/java/tests/Team1TestCase1_Erhan.java
@@ -1,5 +1,15 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import utilities.WebDriverFactory;
+
 public class Team1TestCase1_Erhan {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+    }
 
 }
