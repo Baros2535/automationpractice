f1df9de15ccc8baa43c1827a67e3d3cf82be2da9
savasaksak <savasaksak@gmail.com>
2021-06-30 13:25:51 +0300

    Before method created

--- src/test/java/tests/Team1_TestCase1_Savas.java
+++ src/test/java/tests/Team1_TestCase1_Savas.java
@@ -1,6 +1,15 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import utilities.WebDriverFactory;
+
 public class Team1_TestCase1_Savas {
 
+    WebDriver driver;
 
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+    }
 }
