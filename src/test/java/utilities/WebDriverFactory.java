31d4931f7d7760e04c6d07b525ee15bf9752809c
mrbrooks06 <77938810+mrbrooks06@users.noreply.github.com>
2021-06-30 21:02:28 +0300

    MrBrook's first day completed.

--- src/test/java/utilities/WebDriverFactory.java
+++ src/test/java/utilities/WebDriverFactory.java
@@ -4,29 +4,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.lang.invoke.SwitchPoint;
-import java.util.Locale;
 public class WebDriverFactory {
 
+
     public static WebDriver getDriver(String browserType){
-      WebDriver driver= null;
-        switch (browserType.toLowerCase())
-        {
+        WebDriver driver=null;
+        switch (browserType.toLowerCase ()){
             case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver =new ChromeDriver();
+                WebDriverManager.chromedriver ().setup ();
+                driver=new ChromeDriver ();
                 break;
-            case " firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver=new FirefoxDriver();
+            case "firefox":
+                WebDriverManager.firefoxdriver ().setup ();
+                driver=new FirefoxDriver ();
+
                 break;
 
         }
-
         return driver;
-
     }
+
+
 
 }
