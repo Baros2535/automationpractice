f648591bc80613cc05310d424c25ed2b5103c8fc
Baros2535 <brsaltn.25@gmail.com>
2021-06-30 16:44:40 +0300

    ilk gün testi tamamlandı

--- src/test/java/tests/Baroreis.java
+++ src/test/java/tests/Baroreis.java
@@ -1,6 +1,9 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,8 +13,7 @@ public class Baroreis {
     WebDriver driver;
     @BeforeMethod
     public void setUp(){
-   driver = WebDriverFactory.getDriver("chorome");
-   driver.manage().window().maximize();
+   driver = WebDriverFactory.getDriver("chrome");
 
 
     }
@@ -23,16 +25,38 @@ public class Baroreis {
 
     }
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException {
       // 1. Got to http://automationpractice.com/index.php
-
+    driver.get("http://automationpractice.com/index.php");
+    Thread.sleep(6000);
       // 2. Click your item Add to chart
+        System.out.println("sayfaya girildi");
+    driver.findElement(By.xpath("(//a[@class=\"product_img_link\"])[3]")).click();
+        System.out.println("resim tıklandı");
 
-      // 3. Click Proceed to checkout
+        Thread.sleep(5000);
 
-      // 4. Assert popup page’s total to checkout page’s total amount
+  driver.findElement(By.xpath("//div/a[@title=\"Close\"]")).click();
+        Thread.sleep(300000);
+ driver.findElement(By.xpath("(//div[@class=\"button-container\"])[4]")).click();
+        System.out.println("sepete eklendi");
 
-      // 5. Click proceed to checkout
+        // 3. Assert popup page’s total to checkout page’s total amount
+        Thread.sleep(3000);
+        String expectedPrice = driver.findElement(By.xpath("//div/span[@class=\"ajax_block_cart_total\"]")).getText();
+        System.out.println(expectedPrice);
+
+      // 4. Click Proceed to checkout
+        Thread.sleep(12000);
+    driver.findElement(By.xpath("//a[contains(@title,\"Proceed to checkout\")]")).click();
+Thread.sleep(3000);
+        // 5. Click proceed to checkout
+
+        String actualPrice = driver.findElement(By.cssSelector("#total_price_without_tax")).getText();
+Thread.sleep(3000);
+        Assert.assertEquals(actualPrice,expectedPrice,"Verifying prices");
+
+        //driver.findElement(By.xpath("//div/a[contains(@title,\"Proceed to checkout\")]")).click();
 
 
     }
